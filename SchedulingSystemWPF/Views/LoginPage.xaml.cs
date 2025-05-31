144873560846533b1dce88604fd6a519e6ebecda
Tony Ruiz <tonyruizo@pm.me>
2025-05-31 19:19:03 -0400

    Add login page for authorized users on main window

---
+++ SchedulingSystemWPF/Views/LoginPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+
+namespace SchedulingSystemWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        private readonly Frame _mainFrame;
+
+        public LoginPage(Frame mainFrame)
+        {
+            InitializeComponent();
+            _mainFrame = mainFrame;
+        }
+
+        private void Login_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            string username = UsernameBox.Text;
+            string password = PasswordBox.Password;
+
+        }
+    }
+}
