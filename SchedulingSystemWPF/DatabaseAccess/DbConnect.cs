7396c39d50dfc63550f6073ab5d842e3b1d38459
Tony Ruiz <tonyruizo@pm.me>
2025-05-31 16:47:43 -0400

    Add Data Access

---
+++ SchedulingSystemWPF/DatabaseAccess/DbConnect.cs
@@ -0,0 +1,19 @@
+﻿
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace SchedulingSystemWPF.DatabaseAccess
+{
+    /// <summary>
+    /// Connects to the database.
+    /// </summary>
+    public static class DbConnect
+    {
+        public static MySqlConnection GetConnection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["localdb"].ConnectionString;
+
+            return new MySqlConnection(constr);
+        }
+    }
+}
