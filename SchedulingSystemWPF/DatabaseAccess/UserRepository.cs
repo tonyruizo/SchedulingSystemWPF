f4fa41e9815188cd2f1645759a8075b59728c0ba
Tony Ruiz <tonyruizo@pm.me>
2025-05-31 21:01:53 -0400

    Add User repository to handle user operations

---
+++ SchedulingSystemWPF/DatabaseAccess/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace SchedulingSystemWPF.DatabaseAccess
+{
+    /// <summary>
+    /// A repository handles all operations related to the 'User' table in the MySQL database.
+    /// </summary>
+    public class UserRepository
+    {
+        /// <summary>
+        /// Validate and check if user exist in the database.
+        /// </summary>
+        /// <param name="username">The login username</param>
+        /// <param name="password">The login password</param>
+        /// <returns></returns>
+        public bool ValidateUser(string username, string password)
+        {
+            try
+            {
+                using (var conn = DbConnect.GetConnection())
+                {
+                    conn.Open();
+
+                    // SQL Query
+                    string query = "SELECT COUNT(*) FROM user WHERE userName = @username AND password = @password";
+
+                    using (var cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@username", username);
+                        cmd.Parameters.AddWithValue("@password", password);
+
+                        // Check if record exists
+                        long count = (long)cmd.ExecuteScalar();
+                        return count > 0;
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
