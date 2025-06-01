5bd1729b63bf049995ed3dce0d55277c460feb5a
Tony Ruiz <tonyruizo@pm.me>
2025-05-31 21:02:33 -0400

    Add dashboard functionality and design after user logs in

---
+++ SchedulingSystemWPF/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace SchedulingSystemWPF.ViewModels
+{
+    /// <summary>
+    /// Represents the view model for the dashboard page.
+    /// </summary>
+    /// <remarks>It implements <see cref="INotifyPropertyChanged"/> to support data binding</remarks>
+    public class DashboardViewModel : INotifyPropertyChanged
+    {
+        // Store username from the login process.
+        private readonly string _username;
+
+        /// <summary>
+        /// Initialize constructor.
+        /// </summary>
+        /// <param name="username">Username.</param>
+        public DashboardViewModel(string username)
+        {
+            _username = username;
+        }
+
+        // Display message
+        public string WelcomeUser => $"Hi, {_username}";
+
+        // Binding that updates on property changes.
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
---
+++ SchedulingSystemWPF/Views/DashboardPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace SchedulingSystemWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for DashboardPage.xaml
+    /// </summary>
+    public partial class DashboardPage : Page
+    {
+        public DashboardPage(string username)
+        {
+            InitializeComponent();
+            DataContext = new ViewModels.DashboardViewModel(username);
+        }
+    }
+}
