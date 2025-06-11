223a5051f9b361fc38392a0137d3cc4244fe695f
Tony Ruiz <tonyruizo@pm.me>
2025-06-11 06:49:51 -0400

    Add current logged in user session tracking

---
+++ SchedulingSystemWPF/Services/SessionManager.cs
@@ -0,0 +1,11 @@
+﻿using SchedulingSystemWPF.Models;
+
+namespace SchedulingSystemWPF.Services
+{
+    public static class SessionManager
+    {
+        public static User LoggedInUser { get; set; }
+        public static int UserId => LoggedInUser?.UserId ?? 0;
+        public static string Username => LoggedInUser.UserName ?? "N/A";
+    }
+}
