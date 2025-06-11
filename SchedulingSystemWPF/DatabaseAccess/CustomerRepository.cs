260f200a040502fd11bf49360a9ac8d9107f5b1b
Tony Ruiz <tonyruizo@pm.me>
2025-06-11 06:57:27 -0400

    Update/fix view all customers

--- SchedulingSystemWPF/DatabaseAccess/CustomerRepository.cs
+++ SchedulingSystemWPF/DatabaseAccess/CustomerRepository.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using SchedulingSystemWPF.Models;
 using System.Collections.Generic;
+using System.Windows;
 
 namespace SchedulingSystemWPF.DatabaseAccess
 {
@@ -19,7 +20,29 @@ namespace SchedulingSystemWPF.DatabaseAccess
                 {
                     conn.Open();
 
-                    string cmdQuery = "SELECT * FROM customer;";
+                    string cmdQuery = @"
+                        SELECT 
+                            c.customerId,
+                            c.customerName,
+                            c.addressId,
+                            c.active,
+                            c.createDate,
+                            c.createdBy,
+                            c.lastUpdate,
+                            c.lastUpdateBy,
+
+                            a.address AS AddressLine1,
+                            a.address2 AS AddressLine2,
+                            a.postalCode,
+                            a.phone,
+
+                            ci.city,
+                            co.country
+
+                        FROM customer c
+                        JOIN address a ON c.addressId = a.addressId
+                        JOIN city ci ON a.cityId = ci.cityId
+                        JOIN country co ON ci.countryId = co.countryId";
 
                     using (var cmd = new MySqlCommand(cmdQuery, conn))
                     using (var reader = cmd.ExecuteReader())
@@ -31,6 +54,12 @@ namespace SchedulingSystemWPF.DatabaseAccess
                                 CustomerId = reader.GetInt32("customerid"),
                                 CustomerName = reader.GetString("customerName"),
                                 AddressId = reader.GetInt32("addressId"),
+                                AddressLine1 = reader.GetString("AddressLine1"),
+                                AddressLine2 = reader.GetString("AddressLine2"),
+                                PostalCode = reader.GetString("postalCode"),
+                                Phone = reader.GetString("phone"),
+                                City = reader.GetString("city"),
+                                Country = reader.GetString("country"),
                                 Active = reader.GetBoolean("Active"),
                                 CreateDate = reader.GetDateTime("createDate"),
                                 CreatedBy = reader.GetString("createdBy"),
@@ -52,17 +81,17 @@ namespace SchedulingSystemWPF.DatabaseAccess
         /// <summary>
         /// Add a new customer and add it to the database.
         /// </summary>
-        //    public void AddCustomer()
-        //    {
-        //        try
-        //        {
-
-        //        }
-        //        catch (MySqlException ex)
-        //        {
-        //            throw ex;
-        //        }
-        //    }
+        public void AddCustomer(string customerName, string customerAddress)
+        {
+            try
+            {
+                MessageBox.Show($"Added  {customerName}, and {customerAddress}");
+            }
+            catch (MySqlException ex)
+            {
+                throw ex;
+            }
+        }
 
         //public void EditCustomer()
         //{
