341acc62052fe501b2e89cccd714a8dd1d3da590
Tony Ruiz <tonyruizo@pm.me>
2025-06-13 07:04:09 -0400

    Add City and County Repository for sql operations

---
+++ SchedulingSystemWPF/DatabaseAccess/CityRepository.cs
@@ -0,0 +1,63 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace SchedulingSystemWPF.DatabaseAccess
+{
+    /// <summary>
+    /// Handles all operations related to 'city' table in MySQL database.
+    /// </summary>
+    public class CityRepository
+    {
+        /// <summary>
+        /// Retrieves the unique identifier for a city by its name and country, or inserts a new city if it does not
+        /// exist.
+        /// </summary>
+        /// <remarks>This method either retrieves the ID of an existing city or inserts a new city into
+        /// the database. If the city is inserted, the <paramref name="createdBy"/> parameter is used to track the
+        /// creator.</remarks>
+        /// <param name="cityName">The name of the city to retrieve or insert. Cannot be null or empty.</param>
+        /// <param name="countryId">The unique identifier of the country to which the city belongs. Must be a valid country ID.</param>
+        /// <param name="createdBy">The username or identifier of the user creating the city.</param>
+        /// <returns>The unique identifier of the city. Returns -1 if the operation fails.</returns>
+        public int GetOrInsertCity(string cityName, int countryId, string createdBy)
+        {
+            // Invalid ID / Negative value
+            int cityId = -1;
+
+            using (var conn = DbConnect.GetConnection())
+            {
+                conn.Open();
+
+                var selectCmd = new MySqlCommand("SELECT cityId FROM city WHERE city = @city AND countryId = @countryId;", conn);
+
+                selectCmd.Parameters.AddWithValue("@city", cityName);
+                selectCmd.Parameters.AddWithValue("@countryId", countryId);
+
+                // Return a matching city or null
+                var result = selectCmd.ExecuteScalar();
+
+                if (result != null)
+                {
+                    // If found matching city id, Get the city id
+                    return Convert.ToInt32(result);
+                }
+
+                // Insert and retrive
+                var insertCmd = new MySqlCommand(
+                    @"INSERT INTO city (city, countryId, createDate, createdBy, lastUpdate, lastUpdateBy)
+                  VALUES (@city, @countryId, NOW(), @createdBy, NOW(), @createdBy);
+                  SELECT LAST_INSERT_ID();", conn
+                  );
+
+                insertCmd.Parameters.AddWithValue("@city", cityName);
+                insertCmd.Parameters.AddWithValue("@countryId", countryId);
+                insertCmd.Parameters.AddWithValue("@createdBy", createdBy);
+
+                // Excecute and reassign cityId
+                cityId = Convert.ToInt32(insertCmd.ExecuteScalar());
+            }
+
+            return cityId;
+        }
+    }
+}
---
+++ SchedulingSystemWPF/DatabaseAccess/CountryRepository.cs
@@ -0,0 +1,59 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace SchedulingSystemWPF.DatabaseAccess
+{
+    /// <summary>
+    /// Handles all operations related to 'country' table in MySQL database.
+    /// </summary>
+    public class CountryRepository
+    {
+        /// <summary>
+        /// Retrieves the identifier of the specified country, or inserts a new country if it does not exist.
+        /// </summary>
+        /// <remarks>If the country already exists, its identifier is returned. If the country does not
+        /// exist, it is added to the database and its new identifier is returned. Ensure that <paramref
+        /// name="countryName"/> is unique to avoid unintended behavior.</remarks>
+        /// <param name="countryName">The name of the country to retrieve or insert.</param>
+        /// <param name="createdBy">The name of the user creating the country.</param>
+        /// <returns>The unique identifier of the country. Returns a negative value if the operation fails.</returns>
+        public int GetOrInsertCountry(string countryName, string createdBy)
+        {
+            // Invalid ID / Negative value
+            int countryId = -1;
+
+            using (var conn = DbConnect.GetConnection())
+            {
+                conn.Open();
+
+                var selectCmd = new MySqlCommand("SELECT countryId FROM country WHERE country = @country;", conn);
+
+                selectCmd.Parameters.AddWithValue("@country", countryName);
+
+                // Return a matching country or null
+                var result = selectCmd.ExecuteScalar();
+
+                if (result != null)
+                {
+                    // If found matching country id, Get the country id
+                    return Convert.ToInt32(result);
+                }
+
+                // Insert new country and retrive Id
+                var insertCmd = new MySqlCommand(
+                    @"INSERT INTO country (country, createDate, createdBy, lastUpdate, lastUpdateBy) 
+                      VALUES (@country, NOW(), @createdBy, NOW(), @createdBy);
+                      SELECT LAST_INSERT_ID();", conn
+                    );
+
+                insertCmd.Parameters.AddWithValue("@country", countryName);
+                insertCmd.Parameters.AddWithValue("@createdBy", createdBy);
+
+                // Execute and reassign it to countryId
+                countryId = Convert.ToInt32(insertCmd.ExecuteScalar());
+            }
+
+            return countryId;
+        }
+    }
+}
