e9d1d615826e6d68f9cb00a0648fa5a112eaf7c5
Tony Ruiz <tonyruizo@pm.me>
2025-06-14 13:37:08 -0400

    Fix Add customer form

--- SchedulingSystemWPF/Views/AddCustomerView.xaml.cs
+++ SchedulingSystemWPF/Views/AddCustomerView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows;
+﻿using SchedulingSystemWPF.DatabaseAccess;
+using SchedulingSystemWPF.Services;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace SchedulingSystemWPF.Views
@@ -22,7 +26,73 @@ namespace SchedulingSystemWPF.Views
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Add new");
+            // Assign TextBox values
+            // If !null "?", excecute Trim() to remove white space from start and end
+            string nameInput = CustomerNameBox.Text?.Trim();
+            string address1Input = AddressLine1Box.Text?.Trim();
+            string address2Input = AddressLine2Box.Text?.Trim();
+            string cityInput = CityBox.Text?.Trim();
+            string countryInput = CountryBox.Text?.Trim();
+            string postalCodeInput = PostalCodeBox.Text?.Trim();
+            string phoneInput = PhoneBox.Text?.Trim();
+
+            // Validation if fields are empty
+            if (string.IsNullOrEmpty(nameInput) ||
+                string.IsNullOrEmpty(address1Input) ||
+                string.IsNullOrEmpty(cityInput) ||
+                string.IsNullOrEmpty(countryInput) ||
+                string.IsNullOrEmpty(postalCodeInput) ||
+                string.IsNullOrEmpty(phoneInput))
+            {
+                MessageBox.Show("All fields must be filled.");
+                return;
+            }
+
+            // Validation for string type in name
+            if (int.TryParse(nameInput, out _))
+            {
+                MessageBox.Show("Customer's name cannot be a number.");
+                return;
+            }
+
+            // Validation for phone format pattern (10-digits ignoring non-digits)
+            string phonePattern = @"^\D*(\d\D*){10}$";
+            if (!Regex.IsMatch(phoneInput, phonePattern))
+            {
+                MessageBox.Show("Please enter a valid 10-digits phone number.");
+                return;
+            }
+
+            // Validation for postal code (should be all digits and <= 5 in length)
+            if (!postalCodeInput.All(char.IsDigit) ||
+                postalCodeInput.Length > 5)
+            {
+                MessageBox.Show("Please enter a valid postal code.");
+                return;
+            }
+
+            var createdBy = SessionManager.Username;
+
+            var countryR = new CountryRepository();
+            var cityR = new CityRepository();
+            var addressR = new AddressRepository();
+            var customerR = new CustomerRepository();
+
+            // Get or insert country
+            int countryId = countryR.GetOrInsertCountry(countryInput, createdBy);
+
+            // Get or insert city
+            int cityId = cityR.GetOrInsertCity(cityInput, countryId, createdBy);
+
+            // Get or insert address
+            int addressId = addressR.GetOrInsertAddress(address1Input, address2Input, postalCodeInput, phoneInput, cityId, createdBy);
+
+            // Add Customer
+            customerR.AddCustomer(nameInput, addressId, createdBy);
+
+            MessageBox.Show("Customer has been added!");
+            _parentContainer.Content = new CustomersView(_parentContainer);
+
         }
     }
 }
